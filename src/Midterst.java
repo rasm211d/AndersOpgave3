0c17fa6024ef4572665f891a7aaccbd5ae299a56
Rasmus Jannerup <54896192+rasm211d@users.noreply.github.com>
2019-09-17 10:29:35 +0200

    Initial commit

---
+++ src/Midterst.java
@@ -0,0 +1,6 @@
+public class Midterst {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/Opgave3.java
@@ -0,0 +1,21 @@
+public class Opgave3 {
+    /*Lave en metode midterst, der tager tre tal som parametre.
+    Metoden skal returnere den midterste værdi.
+    F.eks. skal kaldet midterst (6, 10, 8) returnere 8. */
+    public static void main(String[] args) {
+        //Test case hvor sidste værdi er midterst
+        //input: a = 6, b = 10, c = 8
+        //Forventet output: 8
+
+        //Test case hvor første værdi er midterst
+        //Input: a = 3, b = 7, c = 2
+        //Forventet output: 3
+
+        //Test case hvor midterste værdi er midterst
+        //Input: a = 10, b = 25, c = 50
+        //Forventet output: 25
+
+        
+    }
+
+}
